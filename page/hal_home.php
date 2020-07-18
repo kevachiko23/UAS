543ab507bd5b9e860867f6c97191d63bf06188c6
Kevin Glenanda Aditiya <62018139+kevachiko23@users.noreply.github.com>
2020-07-18 19:06:07 +0700

    Add files via upload

---
+++ page/hal_home.php
@@ -0,0 +1,13 @@
+
+<div class="col-md-12">
+          <!-- general form elements -->
+          <div class="box box-primary">
+            <div class="box-header with-border">
+            	      <div class="callout callout-info">
+					        <h4>Welcome </h4>
+					        <p><strong><?php echo $_SESSION['nama']; ?></strong>
+							</p>
+				      </div>
+            </div>
+          </div>
+</div>	
